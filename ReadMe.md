313ebc180a3c854d021e1f57226c499586092be7
jfc3 <jacobboomer@msn.com>
2016-09-30 15:28:27 -0400

    First commit for this project

---
+++ ReadMe.md
@@ -0,0 +1,73 @@
+# John F Croston III Ate Here - Release Notes
+
+* John F Croston III Ate Here - 10K Apart Entry
+* Release Number:  v.0.1.0
+* Release Date: 09-27-2016
+
+## Overview
+The project you are looking at are restauarants [John F Croston III](http://jfciii.com) - ([@jfc3](https://twitter.com/jfc3)) had etean at and his [10K Apart](https://a-k-apart.com/) entry. The website allows people to select a city, state, or nearby location and see what restauarants I recommend in that city or location.
+
+When I started this project, I had been thinking about a more involved version of this application that a person could keep track of all the places they wanted to eat when they travel. More of a personal recommendation or lsit of palces to eat. So this was a way to try a few ideas out and make it quick and easy to display restaurant information, along with being only 10 Kb.
+
+This project uses hand-coded JSON files of cities, states, and nearby locations, which currently are the following:
+
+* Annapolis, MD
+* Austin, TX
+* Baltimore, MD
+* Buffalo, NY
+* Cleveland, PA
+* Columbus, OH
+* Delaware
+* Las Vegas, NV
+* Maryland Suburbs
+* Memphis, TN
+* Mississippi
+* New Orleans, LA
+* New York, NY
+* Northern Virginia Suburbs
+* Ocean City, NJ
+* Philadelphia, PA
+* Portland, OR
+* Rochester, NY
+* San Deigo, CA
+* Syracuse, NY
+* Washington, DC
+
+If you look in the CTY folder, you will find some cities and locations with more than one file. The ones with a dash and the word full (-full) in the name are the full version of the city or location. One of my goals was to create the HTML template and CSS in about 3 - 4Kb, so I had 6 or 7 Kb for content, along with that I wanted the rendered page to be under 10 Kb without minifying or zipping anything. Hopefully, I have done that.
+
+There is a city.josn file that you can use as a tempalte to create other city files.
+
+All the extra CLASSes for each restaurant are there so they have microformats added to them, which is hepful to computers. Look for the 'vcard' CLASS
+
+I'm working on a more detailed version that is not part of the 10K Apart contest that has my recommendations or ones my friends whose food recommendations I trust, TV shows, blog posts, articles, etc.
+
+So in the near future, I plan on creating an application that people can keep track of the places they want to eat when they travel.
+
+## New Features and Enhancements
+Features:
+
+* Most likely add more details about restaurants as I travel along with pay attention to what places I liked and didn't like.
+* Add more cities and more restaurants.
+* Need to break the larger files of ten restaurants or more into multiple files so people don't have to scroll as much places to eat for one city or location.
+* Possibly add an accessible SVG map of the Unites States that will allow people to see places to eat in those states.
+* Will look into adding social media for at least Twitter and Facebook so people can point their friends to the website.
+* Add Google Analytics to see if the full website is getting any traffic.
+
+## Installation
+Please have the following installed before running:
+* You will need a server with at least PHP 5.6
+
+## Versioning notation
+John F Croston III Ate Here public release notation follows the semantic versioning as outlined on http://semver.org
+To summarize,
+Given a version number MAJOR.MINOR.PATCH, increment the:
+
+* MAJOR version for incompatible API changes,
+* MINOR version for backwards-compatible manner alterations and
+* PATCH version for backwards-compatible bug fixes.
+
+Additional labels for pre-release, and build metadata are available as extensions to the MAJOR.MINOR.PATCH format.
+
+## Future Notes
+
+* n/a
---
+++ index.php
@@ -0,0 +1,117 @@
+<?php
+if ($_POST['slct'] != "" && $_POST['slct'] != "xxx") {
+	$json_file = file_get_contents('./cty/'.$_POST['slct'].'.json');
+	$jfo = json_decode($json_file);
+	$city = $jfo->result->city;
+	$posts = $jfo->result->posts;	
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>John F Croston III Ate Here <?php if ($city != "") { echo " in ".$city;} ?></title>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width; initial-scale=1.0" /> 
+	<style>
+		html, body, div, span, h1, h2, h3, p, a, strong, form, label, select { margin: 0; padding: 0; border: 0; outline: 0; font-weight: inherit; font-style: inherit; font-size: 100%; font-family: inherit; vertical-align: baseline;}
+		select, input[type="submit"] { border: solid 1px #000; }
+		select:focus, select:active, input[type="submit"]:focus, input[type="submit"]:active { outline: solid 2px #000;}
+		body { background: #BEFF9E; color: #000; font-family: Helvetica, Verdana, Arial, sans-serif; line-height: 1;}
+		#wrap { margin: 0 auto;}
+		#hdr { background: #ACC7FF;}
+		#cntnt, #ftr { padding: 1em; background: #fff;}
+		#cntnt, #ftr , #ftr { border-top: 1px solid #000;}
+		h1, h2, h3, strong { font-weight: bold;}
+		h2, h3 { margin: .5em 0;}
+		h1 { font-size: 2em; padding: .25em .5em;}
+		h2 { font-size: 1.75em;}
+		h3 { font-size: 1.5em;}
+		a:hover, a:focus, a:active { text-decoration: none;}
+		p { margin: 1em 0; line-height: 1.25em;}
+		a, a:visited { color: #000;}
+		hr { margin: .75em 0 .5em 0;}
+		form { margin: 1em 0;}
+		label, select, input[type="submit"] { padding: .25em 0; font-size: 1.5em;}
+		label { position: relative; display: block;}
+		select { width: 100%; margin: 0 .5em 0 0;}
+		input[type="submit"] { font-size: 1em; font-weight: bold; padding: .5em; background: #BEFF9E; margin: .75em 0 .25em 0;}
+		.scrn-rdr { position: absolute; left: -10000px; top: auto;}		
+		@media screen and (min-width: 32.5em) {
+			#wrp { width: 100%;}
+			#hdr, #cntnt, #ftr { margin: 0 auto; padding: 1em; width: 80%; max-width: 35em; border: 1px solid #000;}
+			#cntnt { padding-top: 0;}
+			#hdr, #cntnt { border-bottom: 0;}
+			h2, h3 { padding: .5em 0 0 0;}
+			h1 { font-size: 2.5em; text-align: center; padding: 0;}
+			h2 { font-size: 2em; padding: 0 .5em 0 0;}
+			h3 { font-size: 1.5em;}
+			p { line-height: 1.5em;}
+			label { display: inline;}
+			select { width: 12.5em;}
+		}
+	</style>
+</head>
+<body>
+<div id="wrap">
+	<div id="hdr" role="banner">
+		<h1>jfciii Ate Here</h1>
+	</div>
+	<div id="cntnt" role="main" tabindex="0">
+		<form name="RequestForm" method="post" action="index.php" role="form">
+			<label for="slct" class="scrn-rdr">Choose a location</label>
+			<select id="slct" name="slct">
+				<option value="xxx">Select a location</option>
+				<option value="ann">Annapolis, MD</option>
+				<option value="aus">Austin, TX</option>
+				<option value="bal">Baltimore, MD</option>
+				<option value="buf">Buffalo, NY</option>
+				<option value="cle">Cleveland, PA</option>
+				<option value="col">Columbus, OH</option>
+				<option value="de">Delaware</option>
+				<option value="las">Las Vegas, NV</option>
+				<option value="md">Maryland Suburbs</option>
+				<option value="mem">Memphis, TN</option>
+				<option value="ms">Mississippi</option>
+				<option value="msy">New Orleans, LA</option>
+				<option value="nyc">New York, NY</option>
+				<option value="nva">Northern Virginia Suburbs</option>
+				<option value="ocnj">Ocean City, NJ</option>
+				<option value="phl">Philadelphia, PA</option>
+				<option value="pdx">Portland, OR</option>
+				<option value="roc">Rochester, NY</option>
+				<option value="san">San Deigo, CA</option>
+				<option value="syr">Syracuse, NY</option>
+				<option value="dca">Washington, DC</option>
+			</select>
+			<input class="submit" type="submit" value="Submit">
+		</form>
+	<?php if ($_POST['slct'] == "" || $_POST['slct'] == "xxx") { ?>
+		<h2>Welcome</h2>
+		<p>Here are places <a href="http://jfciii.com">John F Croston III</a> recommends to eat at when in any of these cities, states, or suburban locations which are part of my <a href="https://a-k-apart.com/">10K Apart</a> entry.</p>
+		<p>In the near future, I plan on creating an application that people can keep use track the places they want to eat when the travel.</p>
+	<?php } else { ?>
+			<h2><?php echo $city; ?></h2>
+			<p>All the restaurants listed are ones I have eaten at and recommend you get to when in these places.</p>
+    <?php foreach ($posts as $post) { ?>
+		<div class="vcard">
+		<?php if ($post->url != "") { ;?>
+			<h3 class="fn"><a href="<?php echo $post->url; ?>" class="url"><?php echo $post->name; ?></a></h3>
+			<?php } else { ?>
+			<h3 class="fn"><?php echo $post->name; ?></h3>
+	    <?php } ?>
+		<p><?php if ($post->note != "") { echo $post->note;} ?></p>
+			<p><strong>Address - </strong><span class="adr"><span class="street-address"><?php echo $post->add1; ?> <?php if ($post->add2 != "") { echo $post->add2.",";} ?></span> <span class="locality"><?php echo $post->cty; ?></span>, <span class="region"><?php echo $post->stt; ?></span> <span class="postal-code"><?php echo $post->zpcd; ?></span></span></p>
+			<?php if ($post->phn != "") { echo "<p><strong>Phone - </strong>".$post->phn."</p>";} ?>
+		</div>
+		<hr>
+    <?php	}	?>
+		<p><a href="#cntnt"><strong>Return to Top of Page</strong></a></p>
+	<?php	}	?>
+		<p>For more restaurants for a given location and a few more cities make sure to check out <a href="http://jfciii.com/atehere/">John F Croston III Ate Here</a>.</p>
+	</div>
+	<div id="ftr" role="contentinfo">
+		<p role="contentinfo">&copy; 2016 - Created by <a href="http://jfciii.com">jfciii.com</a> / <a href="https://twitter.com">@jfc3</a></p>		
+	</div>
+</div>
+</body>
+</html>
